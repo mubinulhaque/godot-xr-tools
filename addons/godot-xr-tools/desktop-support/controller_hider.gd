ab5b57c525743d37c40ec57bac6b6e9138fc8db4
Mubinul Haque <80097192+mubinulhaque@users.noreply.github.com>
2025-12-13 15:38:12 -0100

    Better format Controller Hider

--- addons/godot-xr-tools/desktop-support/controller_hider.gd
+++ addons/godot-xr-tools/desktop-support/controller_hider.gd
@@ -10,7 +10,7 @@ extends Node
 var _pointer_disabler := false
 var _last_xr_active := true
 
-# XRStart node
+## XRStart node
 @onready var xr_start_node: Node = XRTools.find_xr_child(
 		XRTools.find_xr_ancestor(
 				self,
@@ -21,7 +21,7 @@ var _last_xr_active := true
 		"Node",
 )
 
-# Parent controller
+## Parent controller
 @onready var _controller: XRController3D = XRHelpers.get_xr_controller(self)
 
 
@@ -34,30 +34,30 @@ func _ready() -> void:
 
 
 func _process(_delta: float) -> void:
-	if Engine.is_editor_hint() or !is_inside_tree():
+	if Engine.is_editor_hint() or not is_inside_tree():
 		return
-	if xr_start_node.is_xr_active()==_last_xr_active:
+	if xr_start_node.is_xr_active() == _last_xr_active:
 		return
 	if _pointer_disabler:
-		get_parent().enabled=xr_start_node.is_xr_active()
+		get_parent().enabled = xr_start_node.is_xr_active()
 	elif is_instance_valid(_controller):
-		_controller.visible=xr_start_node.is_xr_active()
-	_last_xr_active=xr_start_node.is_xr_active()
+		_controller.visible = xr_start_node.is_xr_active()
+	_last_xr_active = xr_start_node.is_xr_active()
 
 
-# Add support for is_xr_class on XRTools classes
-func is_xr_class(xr_name:  String) -> bool:
+## Add support for is_xr_class on XRTools classes
+func is_xr_class(xr_name: String) -> bool:
 	return xr_name == "XRToolsDesktopControllerHider"
 
 
-# This method verifies the movement provider has a valid configuration.
+## This method verifies the movement provider has a valid configuration.
 func _get_configuration_warnings() -> PackedStringArray:
 	var warnings := PackedStringArray()
 
 	# Check the controller node
 	if (
-			!XRHelpers.get_xr_controller(self)
-			and !XRTools.find_xr_ancestor(self,"*","XRToolsFunctionPointer")
+			not XRHelpers.get_xr_controller(self)
+			and not XRTools.find_xr_ancestor(self, "*", "XRToolsFunctionPointer")
 	):
 		warnings.append("This node must be within a branch of an XRController3D node")
 
