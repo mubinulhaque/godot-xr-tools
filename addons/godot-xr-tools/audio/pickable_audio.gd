a2dc95385cfbffd21438bb2c578139352c010b31
Mubinul Haque <80097192+mubinulhaque@users.noreply.github.com>
2025-12-13 15:40:46 -0100

    Better format Desktop Flight

--- addons/godot-xr-tools/audio/pickable_audio.gd
+++ addons/godot-xr-tools/audio/pickable_audio.gd
@@ -2,8 +2,6 @@
 @icon("res://addons/godot-xr-tools/editor/icons/audio.svg")
 class_name XRToolsPickableAudio
 extends AudioStreamPlayer3D
-
-
 ## XRTools Pickable Audio
 ##
 ## This node is attached as a child of a Pickable,
@@ -20,12 +18,12 @@ extends AudioStreamPlayer3D
 @onready var _pickable: XRToolsPickable = get_parent()
 
 
-# Add support for is_class on XRTools classes
+## Add support for is_class on XRTools classes
 func is_xr_class(xr_name: String) -> bool:
 	return xr_name == "XRToolsPickableAudio"
 
 
-# Called when the node enters the scene tree for the first time.
+## Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# Listen for when this object enters a body
 	_pickable.body_entered.connect(_on_body_entered)
@@ -42,7 +40,7 @@ func _physics_process(_delta: float) -> void:
 			volume_db -= _pickable.linear_velocity.length() * delta_throttle
 
 
-# Called when this object is picked up
+## Called when this object is picked up
 func _on_picked_up(_pickable: XRToolsPickable) -> void:
 	volume_db = 0
 	if playing:
@@ -51,7 +49,7 @@ func _on_picked_up(_pickable: XRToolsPickable) -> void:
 	play()
 
 
-# Called when this object is dropped
+## Called when this object is dropped
 func _on_dropped(_pickable: XRToolsPickable) -> void:
 	for body in _pickable.get_colliding_bodies():
 		if playing:
@@ -68,7 +66,7 @@ func _on_body_entered(_body: Node3D) -> void:
 	play()
 
 
-# This method checks for configuration issues.
+## This method checks for configuration issues.
 func _get_configuration_warnings() -> PackedStringArray:
 	var warnings := PackedStringArray()
 
